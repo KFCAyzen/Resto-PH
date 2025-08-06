d9efce2fcb2e69c50484ac88d748283fc4f8242c
KEPSEU Franck Celestin <kepseufrankc@gmail.com>
2025-08-06 12:51:58 +0200

    Order management debuged

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import './App.css';
 import MenuPage from './MenuPage.tsx';
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import CartPage from './CartPage';
 import logo from './assets/WhatsApp Image 2025-07-29 à 14.49.18_44930011.jpg';
 import type { MenuItem } from './types.ts';
 import { menuItems } from './types.ts';
 import { images } from './images.ts';
 
-function useQuery() {
-  return new URLSearchParams(useLocation().search);
-}
-
 function App() {
   const [cartItems, setCartItems] = useState<MenuItem[]>([]);
-
-  const query = useQuery();
-  const tableNumber = query.get("table");
+  const [tableNumber, setTableNumber] = useState<string | null>(null);
 
   // Charger le panier au démarrage
   useEffect(() => {
@@ -41,45 +34,67 @@ function App() {
 
   // Fonction d’ajout au panier
   const handleAddToCart = (item: MenuItem) => {
-  const existingItem = cartItems.find(i => i.id === item.id);
-  if (existingItem) {
-    const updatedCart = cartItems.map(i =>
-      i.id === item.id ? { ...i, quantité: (i.quantité ?? 0) + 1 } : i
-    );
-    setCartItems(updatedCart);
-  } else {
-    setCartItems([...cartItems, { ...item, quantité: 1 }]);
-  }
-};
+    const existingItem = cartItems.find(i => i.id === item.id);
+    if (existingItem) {
+      const updatedCart = cartItems.map(i =>
+        i.id === item.id ? { ...i, quantité: (i.quantité ?? 0) + 1 } : i
+      );
+      setCartItems(updatedCart);
+    } else {
+      setCartItems([...cartItems, { ...item, quantité: 1 }]);
+    }
+  };
 
+  // Composant avec accès à l'URL
+  function AppContent() {
+    const location = useLocation();
+    const query = new URLSearchParams(location.search);
+    const table = query.get("table");
+
+    useEffect(() => {
+      setTableNumber(table);
+    }, [table]);
+
+    return (
+      <>
+        <div className='title'>
+          <img src={logo} alt="PH" />
+          <h1>PAULINA HÔTEL</h1>
+          <nav className='navbar'>
+            <Link className='menBtn' to="/" style={{ fontWeight: "bold", textDecoration: "none" }}>Menu</Link>
+            <Link className='cartBtn' to="/panier" style={{ fontWeight: "bold", textDecoration: "none", color: "black" }}>
+              <img src={images.cart} />
+              <p>{cartItems.length}</p>
+            </Link>
+          </nav>
+        </div>
+
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <MenuPage
+                cartItems={cartItems}
+                setCartItems={setCartItems}
+                onAddToCart={handleAddToCart}
+              />
+            }
+          />
+          <Route path='/panier' element={
+            <CartPage
+              cartItems={cartItems}
+              setCartItems={setCartItems}
+              tableNumber={tableNumber}
+            />
+          } />
+        </Routes>
+      </>
+    );
+  }
 
   return (
     <Router>
-      <div className='title'>
-        <img src={logo} alt="PH" />
-        <h1>PAULINA HÔTEL</h1>
-        <nav className='navbar'>
-          <Link className='menBtn' to="/" style={{ fontWeight: "bold", textDecoration: "none" }}>Menu</Link>
-          <Link className='cartBtn' to="/panier" style={{ fontWeight: "bold", textDecoration: "none", color: "black" }}>
-            <img src={images.cart} />
-            <p>{cartItems.length}</p>
-          </Link>
-        </nav>
-      </div>
-
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <MenuPage
-              cartItems={cartItems}
-              setCartItems={setCartItems}
-              onAddToCart={handleAddToCart}
-            />
-          }
-        />
-        <Route path='/panier' element={<CartPage cartItems={cartItems} setCartItems={setCartItems} tableNumber = {tableNumber} />} />
-      </Routes>
+      <AppContent />
     </Router>
   );
 }
